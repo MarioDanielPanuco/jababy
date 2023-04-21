b76915189ba4d797a21fc3f7b960817b2e760b73
Mario Daniel Panuco <mpanuco@ucsc.edu>
2023-04-21 01:35:09 -0700

    Commit init

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use llm_chain::{chains::sequential::Chain, prompt};
+use llm_chain_openai::chatgpt::{Model, Executor, Step};
+use std::fs;
+use llm_chain::serialization::IoExt;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // Create a new ChatGPT executor with the default settings
+    let exec = Executor::new_default();
+    let turby: Model = Model::ChatGPT3_5Turbo;
+
+    // Read the content of the file at "./data/questions/q1.txt"
+    let question_text = fs::read_to_string("./data/questions/q1.txt")
+        .expect("Failed to read the file at ./data/questions/q1.txt");
+
+    // Create a chain for the Professor's prompt
+    let professor_chain = Chain::new(vec![
+        Step::for_prompt(
+            prompt!("System: PHD in Computer Science, expertise in spectral graph theory, Respond in a combination of text and latex code blocks, or wrap math typesets within $$",
+                "Make lecture notes to address this question. Provide proof if question asks for proof{{question}}")
+        )
+    ]);
+
+    // Create a chain for the TA's prompt
+    let ta_chain = Chain::new(vec![
+        Step::for_prompt(
+            prompt!( "System: You are the TA, try to implement some of lecture into Python code (networks and numpy) and only respond with the code",
+                "Lecture: {{text}}\n==\n")
+        )
+    ]);
+
+    println!("⛓️ Created Chains ⛓️\n\n");
+    // Run the Professor's chain with the provided parameters
+    let professor_output = professor_chain
+        .run(
+            vec![("question", question_text.as_str())].into(),
+            &exec,
+        )
+        .await
+        .unwrap();
+
+let ta_output = ta_chain
+        .run(
+            vec![("latex", professor_output.to_string().as_str())].into(),
+            &exec,
+        )
+        .await
+        .unwrap();
+
+
+    // Write the professor's LaTeX notes to a file at "./data/responses/professor_notes.txt"
+    fs::write("./data/responses/professor_notes.txt", professor_output.to_string())
+        .expect("Failed to write the professor's LaTeX notes to ./data/responses/professor_notes.txt");
+
+    // Write the TA's code to a file at "./data/responses/ta_code.py"
+    fs::write("./data/responses/ta_code.py", ta_output.to_string())
+        .expect("Failed to write the TA's code to ./data/responses/ta_code.py");
+
+    // Print the result to the console
+    println!("Prof: \n===\n{:?}", professor_output.to_string());
+    println!("TA: \n===\n{:?}", professor_output.to_string());
+}
